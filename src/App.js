683650d565f18cbcf93b2ac0642127ccf67a5ccf
Daniel Rice <Daniel.L.Rice@gmail.com>
2018-10-24 17:10:04 +0100

    My initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import { VennDiagram } from 'venn.js';
+import { select } from 'd3-selection';
+
 
 class App extends Component {
   render() {
+    const A = 'gene:nod2';
+    const B = 'annotation:(type:"alternative products")'
+
+    // Actual numnbers taken from queries
+    const sets = [
+      {sets: [A], size: 191}, 
+      {sets: [B], size: 25230},
+      {sets: [A,B], size: 19}
+    ];
+
+    const setsLog = sets.map((x) => {
+      return {
+        ...x,
+        size: Math.log(x.size),
+      }
+    });
+
+    console.log(JSON.stringify(sets, null, 2));
+    console.log(JSON.stringify(setsLog, null, 2));
+
+    const chart = VennDiagram();
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <div>
+          With standard scaling
+          <svg width={600} height={400}>
+            <g
+              ref={node => select(node).datum(sets).call(chart)}
+            />
+          </svg>
+        </div>
+        <div>
+          With log-scaling
+          <svg width={600} height={400}>
+            <g
+              ref={node => select(node).datum(setsLog).call(chart)}
+            />
+          </svg>
+        </div>
+      </Fragment>
     );
   }
 }
